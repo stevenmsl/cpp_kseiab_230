02b0477ee030278abe1d46e9cc8c96bad3b081a8
Steven Lin <steven_lx@outlook.com>
2022-06-15 14:58:53 -0700

    add more comments

--- solution.cpp
+++ solution.cpp
@@ -16,7 +16,7 @@ using namespace std;
 /*takeaways
   - use inorder traversal as you need to find the
     kth smallest number
-
+  - time complexity: O(n)
 */
 
 int Solution::kthSmallest(Node *root, int k)
@@ -28,11 +28,11 @@ int Solution::_inorder(Node *root, int &k)
 {
   if (root == nullptr)
     return -1;
-  /* need to look at the left first for small values */
+  /* left child is the kth smallest number */
   auto val = _inorder(root->left, k);
   if (k == 0)
     return val;
-  /* the root is the last number you count */
+  /* the root is the kth smallest number */
   if (--k == 0)
     return root->val;
 
